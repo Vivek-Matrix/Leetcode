9d0b0901a1c7e89c64a23875d3b002fb7ddf9e0a
Vivek-Matrix <mannepalli.vivekvardhan@gmail.com>
2026-04-05 22:10:32 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (53.48%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -2,5 +2,5 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         k = k%len(nums)
         if k!=0:
-            nums[:k],nums[k:]=nums[-k:], nums[:-k]   
+            nums[:k],nums[k:]=nums[len(nums)-k:], nums[:len(nums)-k]   
         

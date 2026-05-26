cd540c74af71840daa6abe1d867e6f5749b77562
Vivek-Matrix <mannepalli.vivekvardhan@gmail.com>
2026-05-26 10:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.34%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -3,10 +3,7 @@ class Solution:
         
         if n <= 0:
             return False
-        while n % 2 == 0:
-            n = n // 2
-        while n % 3 == 0:
-            n = n // 3
-        while n % 5 == 0:
-            n = n // 5
-        return n == 1
+        for i in [2,3,5]:
+            while n%i==0:
+                n/=i
+        return n==1

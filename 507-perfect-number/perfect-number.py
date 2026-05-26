f69e053ce81565148b4708df029bbf6a351df100
Vivek-Matrix <mannepalli.vivekvardhan@gmail.com>
2026-05-26 21:29:15 +0530

    Time: 3 ms (90.44%) | Memory: 19 MB (99.54%) - LeetSync

--- 507-perfect-number/perfect-number.py
+++ 507-perfect-number/perfect-number.py
@@ -5,8 +5,7 @@ class Solution:
         s=1
         for i in range(2,int(num**0.5)+1):
             if num%i==0:
-                s+=i
+                s+=i+num//i
 
-                if i != num//i:
-                    s += num//i
+                
         return s == num

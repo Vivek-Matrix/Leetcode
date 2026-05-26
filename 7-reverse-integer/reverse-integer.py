a65e6a51f29cf2acc043fd52621352117d13ffbd
Vivek-Matrix <mannepalli.vivekvardhan@gmail.com>
2026-05-26 11:32:49 +0530

    Time: 48 ms (42.79%) | Memory: 19.3 MB (9.05%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -3,10 +3,10 @@ class Solution:
         sign = -1 if x<0 else 1
         x = abs(x)
         rev=0
-        while x > 0:
+        while x:
             digit = x % 10
             rev = rev * 10 + digit
-            x = x // 10
+            x //= 10
         res = sign*int(rev)
         
 

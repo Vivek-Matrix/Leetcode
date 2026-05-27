9d802f34bdbf7e537dcab0b3b3b35799597fe05d
Vivek-Matrix <mannepalli.vivekvardhan@gmail.com>
2026-05-27 15:38:48 +0530

    Time: 33 ms (98.04%) | Memory: 19.4 MB (10.25%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        s= s.split()
+        l =[""]*len(s)
+        for i in s:
+            index = int(i[-1])-1
+            l[index] = i[:-1]
+        return " ".join(x for x in l)

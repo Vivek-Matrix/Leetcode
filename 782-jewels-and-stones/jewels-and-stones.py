cc077054f103289e34e9b7325dcecdde645dcaac
Vivek-Matrix <mannepalli.vivekvardhan@gmail.com>
2026-05-30 13:43:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.74%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,7 +1,8 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        s=set(jewels)
         c=0
         for i in stones:
-            if i in jewels:
+            if i in s:
                 c+=1
         return c

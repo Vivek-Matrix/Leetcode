1bebd02ae626c74a492754ea316e2548f45f260b
Vivek-Matrix <mannepalli.vivekvardhan@gmail.com>
2026-05-30 14:26:21 +0530

    Time: 11 ms (84.35%) | Memory: 22.2 MB (56.64%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d={}
+        for i in strs:
+            x="".join(sorted(i))
+            if x not in d:
+                d[x] = []
+            d[x].append(i)
+        return list(d.values())
